5c12091c766167bd5311e670e4b53164fb86b99e
Nic Colley <nic.colley@live.com>
2016-04-06 20:11:19 -0400

    First few tests down. *args.... ARGHH!

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -5,12 +5,37 @@ class LinearAlgebra():
         pass
 
     def shape(self, a_list_or_matrix):
-            print(a_list_or_matrix.__class__)
-            num_rows = len(a_list_or_matrix)
-            return (num_rows, )
+        print(a_list_or_matrix.__class__)
+        num_rows = len(a_list_or_matrix)
+        return (num_rows, )
 
-    def vector_add(self, a_list1, a_list2):
-        pass
+    def vector_add(self, a_vector1, a_vector2):
+        if self.shape(a_vector1) == self.shape(a_vector2):
+            temp_vector = []
+            for i, (d, e) in enumerate(zip(a_vector1, a_vector2)):
+                temp_vector.append(d + e)
+            return temp_vector
+        else:
+            raise ShapeError("Two Vectors have different shapes bro")
+
+    def vector_sub(self, a_vector1, a_vector2):
+        if self.shape(a_vector1) == self.shape(a_vector2):
+            temp_vector = []
+            for i, (d, e) in enumerate(zip(a_vector1, a_vector2)):
+                temp_vector.append(d - e)
+            return temp_vector
+        else:
+            raise ShapeError("Two Vectors have different shapes bro")
+
+    def vector_sum(self, *args):
+        lengths =
+        return[sum(a) for a in zip(*args)]
+
+
+
+
+class ShapeError(Exception):
+    pass
 
 if __name__ == '__main__':
     f  = LinearAlgebra()
--- linear_algebra_tests.py
+++ linear_algebra_tests.py
@@ -1,6 +1,8 @@
 import math
-from LinearAlgebra import LinearAlgebra
+from LinearAlgebra import LinearAlgebra, ShapeError
+from LinearAlgebra import *
 from nose.tools import raises
+
 
 def are_equal(x, y, tolerance=0.001):
     """Helper function to compare floats, which are often not quite equal."""
@@ -16,9 +18,6 @@ u = [1, 1, 1]
 y = [10, 20, 30]
 z = [0, 0, 0]
 
-A = [[1, 0, 0],
-     [0, 1, 0]]
-
 
 def test_shape_vectors():
     f = LinearAlgebra()
@@ -28,7 +27,6 @@ def test_shape_vectors():
     assert f.shape([1]) == (1,)
     assert f.shape(m) == (2,)
     assert f.shape(v) == (3,)
-
 
 def test_vector_add():
     f = LinearAlgebra()
@@ -42,102 +40,106 @@ def test_vector_add():
     assert f.vector_add(u, z) == u
 
 
-#
-#
-# def test_vector_add_is_commutative():
-#     assert vector_add(w, y) == vector_add(y, w)
-#
-#
-# @raises(ShapeError)
-# def test_vector_add_checks_shapes():
-#     """Shape rule: the vectors must be the same size."""
-#     vector_add(m, v)
-#
-#
-# def test_vector_sub():
-#     """
-#     [a b]  - [c d]  = [a-c b-d]
-#     Matrix + Matrix = Matrix
-#     """
-#     assert vector_sub(v, w) == [1, 1, -4]
-#     assert vector_sub(w, v) == [-1, -1, 4]
-#     assert vector_sub(y, z) == y
-#     assert vector_sub(w, u) == vector_sub(z, vector_sub(u, w))
-#
-#
-# @raises(ShapeError)
-# def test_vector_sub_checks_shapes():
-#     """Shape rule: the vectors must be the same size."""
-#     vector_sub(m, v)
-#
-#
-# def test_vector_sum():
-#     """vector_sum can take any number of vectors and add them together."""
-#     assert vector_sum(v, w, u, y, z) == [12, 26, 35]
-#
-#
+def test_vector_add_is_commutative():
+    f = LinearAlgebra()
+    assert f.vector_add(w, y) == f.vector_add(y, w)
+
+
+@raises(ShapeError)
+def test_vector_add_checks_shapes():
+    f = LinearAlgebra()
+    """Shape rule: the vectors must be the same size."""
+    f.vector_add(m, v)
+
+
+def test_vector_sub():
+    """
+    [a b]  - [c d]  = [a-c b-d]
+    Matrix + Matrix = Matrix
+    """
+    f = LinearAlgebra()
+    assert f.vector_sub(v, w) == [1, 1, -4]
+    assert f.vector_sub(w, v) == [-1, -1, 4]
+    assert f.vector_sub(y, z) == y
+    assert f.vector_sub(w, u) == f.vector_sub(z, f.vector_sub(u, w))
+
+
+@raises(ShapeError)
+def test_vector_sub_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    f = LinearAlgebra()
+    f.vector_sub(m, v)
+
+
+def test_vector_sum():
+    f = LinearAlgebra()
+
+    """vector_sum can take any number of vectors and add them together."""
+    assert f.vector_sum(v, w, u, y, z) == [12, 26, 35]
+
+
 # @raises(ShapeError)
 # def test_vector_sum_checks_shapes():
 #     """Shape rule: the vectors must be the same size."""
 #     vector_sum(v, w, m, y)
-#
-#
-# def test_dot():
-#     """
-#     dot([a b], [c d])   = a * c + b * d
-#     dot(Vector, Vector) = Scalar
-#     """
-#     assert dot(w, y) == 160
-#     assert dot(m, n) == 15
-#     assert dot(u, z) == 0
-#
-#
-# @raises(ShapeError)
-# def test_dot_checks_shapes():
-#     """Shape rule: the vectors must be the same size."""
-#     dot(v, m)
-#
-#
-# def test_vector_multiply():
-#     """
-#     [a b]  *  Z     = [a*Z b*Z]
-#     Vector * Scalar = Vector
-#     """
-#     assert vector_multiply(v, 0.5) == [0.5, 1.5, 0]
-#     assert vector_multiply(m, 2) == [6, 8]
-#
-#
-# def test_vector_mean():
-#     """
-#     mean([a b], [c d]) = [mean(a, c) mean(b, d)]
-#     mean(Vector)       = Vector
-#     """
-#     assert vector_mean(m, n) == [4, 2]
-#     assert vector_mean(v, w) == [0.5, 2.5, 2]
-#     assert are_equal(vector_mean(v, w, u)[0], 2 / 3)
-#     assert are_equal(vector_mean(v, w, u)[1], 2)
-#     assert are_equal(vector_mean(v, w, u)[2], 5 / 3)
-#
-#
-# def test_magnitude():
-#     """
-#     magnitude([a b])  = sqrt(a^2 + b^2)
-#     magnitude(Vector) = Scalar
-#     """
-#     assert magnitude(m) == 5
-#     assert magnitude(v) == math.sqrt(10)
-#     assert magnitude(y) == math.sqrt(1400)
-#     assert magnitude(z) == 0
-#
-#
-# A = [[1, 0, 0],
-#      [0, 1, 0],
-#      [0, 0, 1]]
-# B = [[1, 2, 3],
-#      [4, 5, 6],
-#      [7, 8, 9]]
-# C = [[1, 2],
-#      [2, 1],
-#      [1, 2]]
-# D = [[1, 2, 3],
-#      [3, 2, 1]]
+    #
+    #
+    # def test_dot():
+    #     """
+    #     dot([a b], [c d])   = a * c + b * d
+    #     dot(Vector, Vector) = Scalar
+    #     """
+    #     assert dot(w, y) == 160
+    #     assert dot(m, n) == 15
+    #     assert dot(u, z) == 0
+    #
+    #
+    # @raises(ShapeError)
+    # def test_dot_checks_shapes():
+    #     """Shape rule: the vectors must be the same size."""
+    #     dot(v, m)
+    #
+    #
+    # def test_vector_multiply():
+    #     """
+    #     [a b]  *  Z     = [a*Z b*Z]
+    #     Vector * Scalar = Vector
+    #     """
+    #     assert vector_multiply(v, 0.5) == [0.5, 1.5, 0]
+    #     assert vector_multiply(m, 2) == [6, 8]
+    #
+    #
+    # def test_vector_mean():
+    #     """
+    #     mean([a b], [c d]) = [mean(a, c) mean(b, d)]
+    #     mean(Vector)       = Vector
+    #     """
+    #     assert vector_mean(m, n) == [4, 2]
+    #     assert vector_mean(v, w) == [0.5, 2.5, 2]
+    #     assert are_equal(vector_mean(v, w, u)[0], 2 / 3)
+    #     assert are_equal(vector_mean(v, w, u)[1], 2)
+    #     assert are_equal(vector_mean(v, w, u)[2], 5 / 3)
+    #
+    #
+    # def test_magnitude():
+    #     """
+    #     magnitude([a b])  = sqrt(a^2 + b^2)
+    #     magnitude(Vector) = Scalar
+    #     """
+    #     assert magnitude(m) == 5
+    #     assert magnitude(v) == math.sqrt(10)
+    #     assert magnitude(y) == math.sqrt(1400)
+    #     assert magnitude(z) == 0
+    #
+    #
+    # A = [[1, 0, 0],
+    #      [0, 1, 0],
+    #      [0, 0, 1]]
+    # B = [[1, 2, 3],
+    #      [4, 5, 6],
+    #      [7, 8, 9]]
+    # C = [[1, 2],
+    #      [2, 1],
+    #      [1, 2]]
+    # D = [[1, 2, 3],
+    #      [3, 2, 1]]
